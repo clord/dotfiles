0fe71df5ba0c2353d2386b9b3cfc7b7c32439d74
Christopher Lord <christopherlord@gmail.com>
2011-02-07 12:40:37 -0500

    add my xmonad configuration

---
+++ xmonad/lib/ChrisConfig.hs
@@ -0,0 +1,107 @@
+module ChrisConfig where
+
+import Data.List(unfoldr, intersperse)
+import Data.Char
+
+type FontSize = Integer
+type FontSpec = String
+data BgColor = Red | Orange | Green | Blue | White 
+             | Black | Brown | Cyan 
+             | Custom Integer Integer Integer
+data Machine = Pape | Bloor | Brimley
+               deriving Show
+data MyFont = Inconsolata | DejaVue | Liberation
+data FgOrBg = Foreground | Background
+
+
+-- total function implementing a saturating 0..255 range of colour strings in hex format
+intToHex :: Integral a => a -> String
+intToHex n
+     | n <= 0    = "00"
+     | n >= 255  = "ff"
+     | n < 16    = '0' : int_to_dig n
+     | otherwise = int_to_dig n
+    where itoh   = reverse . unfoldr (\x -> if x == 0 then Nothing 
+                                                      else let (a, b) = x `quotRem` 16 
+                                                            in Just (b, a))
+          int_to_dig n = map intToDigit $ itoh $ fromIntegral n
+
+makeSpec :: MyFont -> FontSize -> FontSpec 
+makeSpec Inconsolata s = "Inconsolata:size=" ++ (show s)
+makeSpec DejaVue     s = "DejaVue Sans Mono:size=" ++ (show s)
+makeSpec Liberation  s = "Liberation Mono:size=" ++ (show s)
+
+showColor :: FgOrBg -> Integer -> Integer -> Integer -> String
+showColor a r g b = case a of
+                      Foreground -> "-fg '#" ++ digits ++ "'"
+                      Background -> "-bg '#" ++ digits ++ "'"
+                    where digits = (intToHex r) ++ (intToHex g) ++ (intToHex b)
+
+colorstr :: Integer -> Integer -> Integer -> String
+colorstr r' g' b' = (showColor Background r' g' b')  ++ " " ++ 
+                    (showColor Foreground (r'+218) (g'+218) (b'+218))
+
+colorToStrings :: BgColor -> String 
+colorToStrings White  = (showColor Background 255 255 255) ++ " " ++ (showColor Foreground 0 0 60)
+colorToStrings Red    = colorstr 30   0   0
+colorToStrings Cyan   = colorstr  0  30  40
+colorToStrings Green  = colorstr  0  25   0
+colorToStrings Blue   = colorstr  0   0  40
+colorToStrings Orange = colorstr 50  25   0
+colorToStrings Brown  = colorstr 25  18   0
+colorToStrings (Custom r g b) = colorstr r g b
+colorToStrings _      = colorstr 0 0 0
+
+
+bgColorTriplet :: (Integer, Integer, Integer) -> BgColor
+bgColorTriplet (a, b, c) = Custom a b c
+
+
+getColoredTerm :: BgColor -> FontSpec -> String
+getColoredTerm c s = "xterm -fa '" ++ s ++ "' " ++ (colorToStrings c)
+
+getTerminal :: FontSpec -> String
+getTerminal = getColoredTerm Black
+
+
+csOne :: Int -> BgColor
+csOne = (!!) [ Custom 47 21 00
+             , Custom 47 31 30
+             , Custom 47 00 00
+             , Custom 61 22 22 ]
+
+csTwo :: Int -> BgColor
+csTwo = (!!) [ Custom 08 26 28
+             , Custom 09 15 31
+             , Custom 09 36 13
+             , Custom 46 28 12 ]
+
+csThree :: Int -> BgColor
+csThree i = bgColorTriplet ((zip3 (repeat 80) [20,25..] [10,15..]) !! i)
+
+
+wrapAndJoin :: [String] -> String
+wrapAndJoin a = '(' : (concat (intersperse "; " a)) ++ ")&"
+
+csForMachine :: Machine -> Int -> BgColor
+csForMachine Brimley = csOne
+csForMachine   Bloor = csTwo
+csForMachine    Pape = csThree
+csForMachine       _ = error "can't find scheme for machine"
+
+screenCommand :: Machine -> String
+screenCommand    Pape = "/home/clord/Linux/bin/screen"
+screenCommand Brimley = "/home/clord/Linux/bin/screen"
+screenCommand   Bloor = "/home/clord/AIX/bin/screen"
+screenCommand       _ = error "can't find screen command for machine"
+
+screenOn :: Machine -> Int -> String
+screenOn m s = "TERM=xterm /usr/bin/ssh -XC clord@" ++ (show m) ++ 
+               " -t " ++ (screenCommand m) ++ " -A -xR " ++ mAndS ++ 
+               " -c '/home/clord/etc/screen/" ++ mAndS ++ ".screenrc'"
+         where mAndS = (map toLower $ show m) ++ (show s)
+
+notifyCmd :: String -> String -> String
+notifyCmd a o = "DISPLAY=durden:0 notify-send -u low '" ++ a ++ "' '" ++ o ++ "'"
+
+
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,103 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.StackSet as W
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Config.Desktop(desktopConfig)
+import XMonad.Actions.SpawnOn(spawnHere, mkSpawner, Spawner)
+import XMonad.Hooks.SetWMName(setWMName)
+import XMonad.Layout.PerWorkspace as PW
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Spiral
+import XMonad.Layout.Circle
+import XMonad.Layout.Accordion
+import XMonad.Layout.NoBorders
+import Data.Char
+import System.IO
+import ChrisConfig
+
+wsn = [":tx", ":ed" ,":tm", ":nt", ":wb", ":vc", ":tp", ":ei", ":im"]
+ws = zipWith (:) ['1'..] wsn
+
+defaultFont = Liberation
+
+
+localLayoutHook = smartBorders $ avoidStruts $ 
+                    PW.onWorkspaces [ws!!3, ws!!4, ws!!5] (noBorders simpleTabbed)  $ 
+                        Tall 1 0.03 0.5 ||| noBorders Full ||| spiral (6/7) ||| 
+                        Accordion ||| noBorders Circle ||| (noBorders simpleTabbed)
+
+main :: IO ()
+main = do
+          sp <- mkSpawner
+          spawn "/usr/bin/gnome-settings-daemon"
+          xmobar <- spawnPipe "xmobar ~/.xmobarrc"
+          spawn "empathy"
+          spawn "/home/clord/.dropbox-dist/dropboxd"
+          xmonad $ desktopConfig {
+                 borderWidth        = 1
+               , normalBorderColor  = "grey10"
+               , startupHook        = setWMName "LG3D"
+               , focusedBorderColor = "gray70"
+               , XMonad.workspaces  = ws
+               , manageHook         = manageDocks <+> zManageHook <+> manageHook defaultConfig
+               , layoutHook         = localLayoutHook
+               , modMask            = mod4Mask
+               , terminal           = getTerminal $ smlFont
+               , logHook            = dynamicLogWithPP $ xmobarPP {
+                                                  ppOutput = hPutStrLn xmobar,
+                                                  ppTitle = xmobarColor "green" "" . shorten 90 }
+             } `additionalKeys`
+              ([m4  xK_a  $ spawnEditorIn sp "Asti" "/media/bloor/asti"
+               ,m4  xK_r  $ spawnEditorIn sp "Root" "/media/bloor"
+               ,m4  xK_n  $ spawn "/opt/ibm/lotus/notes/notes"
+               ,m4  xK_v  $ spawn "/usr/bin/nvidia-settings"
+               ,m4  xK_g  $ spawn "/usr/bin/google-chrome"
+               ,m4  xK_s  $ spawn "/usr/bin/gnome-screensaver-command -a"
+               ,m4  xK_h  $ spawn "/usr/bin/cmvc-client-gui"
+               ,m1  xK_w  kill
+               ] ++
+                (term_launchers sp Bloor   [xK_F1, xK_F2, xK_F3, xK_F4]) ++
+                (term_launchers sp Pape    [xK_F9]) ++
+                (term_launchers sp Brimley [xK_F5, xK_F6, xK_F7, xK_F8]))
+         where smlFont = makeSpec defaultFont 11
+               m4 a = (,) (mod4Mask, a)
+               m1 a = (,) (mod1Mask, a)
+               m4s a = (,) ((mod4Mask .|. shiftMask), a)
+               rta sp machine font tid = runInXTerm sp color font cmd
+                                 where cmd = screenOn machine tid
+                                       color = csForMachine machine $ tid - 1
+               term_launchers sp machine keys = [makepair kb md | md <- idxkeys, kb <- keyfnt]
+                                          where idxkeys = zip keys [1..]
+                                                keyfnt = zip [smlFont,  makeSpec defaultFont 18]
+                                                             [mod4Mask, mod4Mask .|. shiftMask]
+                                                makepair kb md = ((snd kb, fst md),
+                                                                  rta sp machine (fst kb) (snd md))
+
+
+
+
+
+runInXTerm :: Spawner -> BgColor -> FontSpec -> String -> X ()
+runInXTerm sp color font e = spawnHere sp $ (getColoredTerm color font) ++ " -e \"" ++ e ++ "\""
+
+spawnEditorIn :: Spawner -> String -> String -> X ()
+spawnEditorIn sp e s = spawnHere sp $ wrapAndJoin [notifyCmd (e ++ "Editor") ("in " ++ s), 
+                                                   "cd " ++ s, "gvim"]
+
+zManageHook = composeAll [ className =? "Vncviewer" --> doFloat
+                         , className =? "Firefox" --> doF (W.shift $ ws!!4)
+                         , className =? "Google-chrome" --> doF (W.shift $ ws!!4)
+                         , (resource =? "RootWindow" <&&> className =? "Totalview") --> (doF(W.shift $ ws!!8) <+> doFloat)
+                         , (resource =? "dataTableWindow" <&&> className =? "Totalview") --> doFloat
+                         , className =? "com-ibm-sdwb-cmvc-client-dc-CMVC" --> doF (W.shift $ ws!!5)
+                         , resource =? "gecko"  --> doFloat 
+                         , resource =? "gcalctool"  --> doFloat 
+                         , resource =? "empathy" --> doFloat 
+                         , resource =? "pidgin" --> doFloat 
+                         , className =? "Sametime" --> doFloat 
+                         , className =? "Lotus Notes" --> doF W.focusDown --- This prevents focus stealing
+                         ]
+
+
