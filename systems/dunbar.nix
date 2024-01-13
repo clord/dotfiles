d22a94ae1c4483a2a237a9da8c76f556c84989f6
Christopher Lord <christopher@pliosoft.com>
2024-01-12 23:03:33 -0700

    file system

--- systems/dunbar.nix
+++ systems/dunbar.nix
@@ -1,18 +1,20 @@
-{config, lib, pkgs, ...}: {
+{config, lib, pkgs, modulesPath, ...}: {
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
   boot.initrd.availableKernelModules = [ "xhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+  fileSystems = { 
+    "/" = {
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
+      };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/2178-694E";
+      fsType = "vfat";
     };
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/2178-694E";
-      fsType = "fat";
-    };
+  };
   swapDevices = [ ];
   networking.useDHCP = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
