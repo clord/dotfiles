84206def8ed932879257f1ca1276a047990d4cc1
Christopher Lord <christopher@pliosoft.com>
2024-01-16 17:28:03 -0700

    secrets

--- pubkeys/default.nix
+++ pubkeys/default.nix
@@ -3,13 +3,16 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINXLYw43gNlnfEoHpmK/UWae4DcQyLBQTGQH9ZYlRG5q" # clord@wildwood
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINLtiIXQ0r+l0gtnjCj1hT5Z1YzRqgJ/g66pP/eEuXM3" # clord@ipad
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHTOl4xwPOT82EmW5bEBpWyi5Iy9ZEYWPToJEQjIagyO" # clord@1p
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbu/FXxZhyXOEsQpsAm2YLR4P07WoFcYTm1tbUohQ1U" # clord@dunbar
   ];
   hosts = {
     edmon = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG44RTSc6mqKI+9F0SbP9Qstk1xVg1FAALK/sVCDHaj/" ];
     chickenpi = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO6l/tsNUalrBFf0Zaftlk0QqYTQZKMfomJxv1dt7a/1" ];
     jasper = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO6l/tsNUalrBFf0Zaftlk0QqYTQZKMfomJxv1dt7a/1" ];
+    dunbar = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHuh/pr52sE9A0KJC1wUsE+z/TVdOOR38bSjNqjkbqTU" ];
   };
-in {
+in
+{
   clord = {
     inherit user;
     computers = user ++ (builtins.foldl' (a: b: a ++ b) [ ] (builtins.attrValues hosts)); # everything
