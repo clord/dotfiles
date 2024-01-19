a257655f5b347a53c9fa4b026554945729b83670
Christopher Lord <christopher@lord.ac>
2024-01-18 19:55:21 -0700

    lock new version

--- home/clord/fish.nix
+++ home/clord/fish.nix
@@ -9,7 +9,7 @@
           owner = "IlanCosman";
           repo = "tide";
           rev = "a34b0c2809f665e854d6813dd4b052c1b32a32b4";
-          sha256 = "sha256-xqbG996HFjLHLVkLAqA8sBuhjofVSCbgmnYKK7fKw1o=";
+          sha256 = "sha256-ZyEk/WoxdX5Fr2kXRERQS1U1QHH3oVSyBQvlwYnEYyc=";
         };
       }
 
