9c15ad040901af2bd90b0900e47aaddbbeb5b96a
Christopher Lord <christopher@pliosoft.com>
2025-06-02 15:23:38 -0600

    add screenshots back in

--- systems/darwin-common.nix
+++ systems/darwin-common.nix
@@ -12,17 +12,46 @@
     defaults = {
       spaces.spans-displays = false;
       alf.globalstate = 1;
+
       WindowManager = {
-        GloballyEnabled = true;
+        GloballyEnabled = false;
         EnableStandardClickToShowDesktop = false;
         StandardHideDesktopIcons = true;
         EnableTilingByEdgeDrag = false;
         EnableTopTilingByEdgeDrag = false;
         EnableTilingOptionAccelerator = true;
       };
+
       ".GlobalPreferences" = {
         "com.apple.mouse.scaling" = -1.0;
       };
+
+      CustomUserPreferences = {
+        "com.apple.desktopservices" = {
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+
+        "com.apple.screensaver" = {
+          askForPassword = 1;
+          askForPasswordDelay = 2;
+        };
+
+        "com.apple.SoftwareUpdate" = {
+          AutomaticCheckEnabled = true;
+          ScheduleFrequency = 1;
+          AutomaticDownload = 1;
+          CriticalUpdateInstall = 1;
+        };
+
+        # Turn on app auto-update
+        "com.apple.commerce".AutoUpdate = true;
+
+        # Prevent Photos from opening automatically when devices are plugged in
+        "com.apple.ImageCapture".disableHotPlug = true;
+
+      };
+
       dock = {
         autohide = false;
         orientation = "left";
@@ -33,7 +62,7 @@
       };
 
       screencapture = {
-        location = "$HOME/Desktop";
+        location = "~/Screenshots";
         disable-shadow = true;
       };
       finder = {
