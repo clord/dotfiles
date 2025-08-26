b2cc142f94edd38753c8d509a7d400edfcdf3517
Christopher Lord <christopher@pliosoft.com>
2025-08-26 13:41:30 -0600

    one mreo lint

--- home/eugene/common.nix
+++ home/eugene/common.nix
@@ -1,11 +1,11 @@
 _: {
-  imports = [];
+  imports = [ ];
   home.stateVersion = "24.05";
-  programs.bat.enable = true;
-  programs.ssh = import ./ssh.nix;
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  home.packages = [];
+  programs = {
+    bat.enable = true;
+    ssh = import ./ssh.nix;
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+    packages = [ ];
+  };
 }
